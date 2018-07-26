e2d14a90fa46f4787a43aaa748ddd4e2e4f7db68
Serge A. Salamanka <salamanka.serge@gmail.com>
2018-07-26 15:45:29 +0300

    move bluepill dependency to a wrapper cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'elijah.wright@gmail.com'
 license          'Apache-2.0'
 description      'Installs/Configures Prometheus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.6.5'
+version          '0.6.6'
 source_url 'https://github.com/elijah/chef-prometheus'
 issues_url 'https://github.com/elijah/chef-prometheus/issues'
 chef_version '>= 12.15.25' if respond_to?(:chef_version)
@@ -21,5 +21,7 @@ depends 'ark'
 depends 'golang'
 depends 'compat_resource'
 
-# this should really be a suggests
-depends 'bluepill', '~> 2.3'
+# if you need to use bluepill just include it
+# as a dependency in your wrapper cookbook
+# and set appropriate attributes
+#depends 'bluepill', '~> 2.3'
